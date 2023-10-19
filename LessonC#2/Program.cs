744920b0c02462ef3d478570e5170cc7a7d21ef3
Julia-Tryan <uliana.troyan@mail.ru>
2023-10-19 22:06:12 +0300

    Вторая задача

--- LessonC#2/Program.cs
+++ LessonC#2/Program.cs
@@ -4,25 +4,66 @@
 // Х не = 0 и Y не =0 и выдаёт номер четверти
 // плоскости, в которой находится точка.
 
-Console.WriteLine("Введите число: ");
-int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// int x = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число: ");
-int y = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// int y = int.Parse(Console.ReadLine());
 
-if (x > 0 && y > 0)
-{
-    Console.WriteLine("1");
-}
-else if(x < 0 && y > 0)
-{
-    Console.WriteLine("2");
-}
-else if(x < 0 && y < 0)
-{
-    Console.WriteLine("3");
-}
-else 
-{
-    Console.WriteLine("4");
-}
+// if (x > 0 && y > 0)
+// {
+//     Console.WriteLine("1");
+// }
+// else if(x < 0 && y > 0)
+// {
+//     Console.WriteLine("2");
+// }
+// else if(x < 0 && y < 0)
+// {
+//     Console.WriteLine("3");
+// }
+// else 
+// {
+//     Console.WriteLine("4");
+// }
+
+// Задача №18
+// Напишите программу, которая по заданному номеручетверти,
+// показывает диапазон возможности координат точек в этой четверти (x и y).
+
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// switch (num)
+// {
+//     case 1:
+//         {
+
+//             Console.WriteLine("x<0  y>0");
+//             break;
+//         }
+//     case 2:
+//         {
+
+//             Console.WriteLine("x<0  y>0");
+//             break;
+//         }
+//     case 3:
+//         {
+
+//             Console.WriteLine("x<0  y<0");
+//             break;
+//         }
+//     case 4:
+//         {
+//             Console.WriteLine("x>0  y<0");
+//             break;
+//         }
+//         default:
+//         {
+//             Console.WriteLine("Нет такой четверти");
+//             break;
+//         }
+// }
+
+// 
