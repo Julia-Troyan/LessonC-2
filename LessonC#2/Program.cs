0543756ea1297793913e7400d2aad8b80309b65c
Julia-Tryan <uliana.troyan@mail.ru>
2023-10-19 23:24:51 +0300

    Четвертая задача

--- LessonC#2/Program.cs
+++ LessonC#2/Program.cs
@@ -101,3 +101,27 @@
 //         }
 //     }
 // }
+
+// Напишите программу, котрая принимает на вход
+// координаты двух точек и находит расстояние между
+// ними в 2D пространстве.
+// *A(3,6); B(2,1)->5,9
+// *A(7,-5); B(1,-1)->7,21
+
+
+Console.WriteLine("Введите число: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+
+double s = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+Console.WriteLine(Math.Round(s,2));
